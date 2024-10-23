c6e6689e72018c838ef76876a2d13f3a089e3d19
Miqueas (Sozhia) García González <coding4canary@gmail.com>
2024-10-23 13:33:52 +0100

    Added Class UserController.java

---
+++ src/main/java/com/miqueas/curso/springboot/webapp/springboot_webuno/User/Controller/UserController.java
@@ -0,0 +1,8 @@
+package com.miqueas.curso.springboot.webapp.springboot_webuno.User.Controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class UserController {
+
+}
