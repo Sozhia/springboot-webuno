e0b07aca01e50f4edf146f56c32dc9d4f35f9cb2
Miqueas (Sozhia) García González <coding4canary@gmail.com>
2024-10-23 13:39:12 +0100

    Details template defined in method GetMapping

--- src/main/java/com/miqueas/curso/springboot/webapp/springboot_webuno/User/Controller/UserController.java
+++ src/main/java/com/miqueas/curso/springboot/webapp/springboot_webuno/User/Controller/UserController.java
@@ -1,12 +1,14 @@
 package com.miqueas.curso.springboot.webapp.springboot_webuno.User.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
 
+    @GetMapping("/details")
     public String details(){
-        return "Detalles de plantilla";
+        return "details";
     }
 
 }
