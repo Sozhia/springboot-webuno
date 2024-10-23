9343d55ac6e3ea7f209243db87951c36de96f641
Miqueas (Sozhia) García González <coding4canary@gmail.com>
2024-10-23 15:15:53 +0100

    Details template defined in method GetMapping. Output expected at /details url

--- src/main/java/com/miqueas/curso/springboot/webapp/springboot_webuno/User/Controller/UserController.java
+++ src/main/java/com/miqueas/curso/springboot/webapp/springboot_webuno/User/Controller/UserController.java
@@ -1,13 +1,20 @@
 package com.miqueas.curso.springboot.webapp.springboot_webuno.User.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Map;
 
 @Controller
 public class UserController {
 
     @GetMapping("/details")
-    public String details(){
+    public String details(Map<String, Object> model){
+        model.put("title", "Valor de atributo");
+        model.put("name", "Andrew");
+        model.put("lastname", "Bishop");
+
         return "details";
     }
 
