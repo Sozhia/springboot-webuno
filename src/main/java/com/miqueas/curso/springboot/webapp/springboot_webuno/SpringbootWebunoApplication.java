64f5f170c15a4ecc77c8e3515da7f3edcae910f0
MiqueasGG <mqggonzalez@gmail.com>
2024-10-23 12:49:41 +0100

    Initial setup

---
+++ src/main/java/com/miqueas/curso/springboot/webapp/springboot_webuno/SpringbootWebunoApplication.java
@@ -0,0 +1,13 @@
+package com.miqueas.curso.springboot.webapp.springboot_webuno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWebunoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWebunoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/miqueas/curso/springboot/webapp/springboot_webuno/SpringbootWebunoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miqueas.curso.springboot.webapp.springboot_webuno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWebunoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
